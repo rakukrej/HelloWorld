76aa6e15e01f13c87888329db5bb27af6c1ae103
Rashmil Kukreja <69751233+rakukrej@users.noreply.github.com>
2020-10-06 14:41:36 +0530

    change
    change

---
+++ src/test/java/com/pega/practice/HelloWorld/controller/HelloWorldControllerTest.java
@@ -0,0 +1,21 @@
+package com.pega.practice.HelloWorld.controller;
+
+
+import org.junit.jupiter.api.Test;
+import com.pega.practice.HelloWorld.Controller.HelloWorldController;
+
+public class HelloWorldControllerTest {
+	
+	@Test
+    public void isGreaterTest() {
+        System.out.println("Test");
+        HelloWorldController helloWorld = new HelloWorldController();
+
+        org.junit.jupiter.api.Assertions.assertTrue( helloWorld.hello(4, 3));
+        org.junit.jupiter.api.Assertions.assertFalse(helloWorld.hello(3, 4));
+    }
+	
+
+
+
+}
